7555660d46573d90f582cd64ee8969b128193cb9
Hasan Khan <slashdot10@gmail.com>
2014-08-21 07:43:36 -0700

    * moved analytics to top * factored out css and js into separate files

---
+++ site.js
@@ -0,0 +1,382 @@
+var tagsMRU = [];
+var tagsRecentlyAdded = [];
+var context = null;
+
+function doAddTag(val, surahNum, verseNum) {
+	var deferred = $.Deferred();
+	
+	console.log("Adding tag: " + val + " to " + "[" + surahNum + ":" + verseNum + "]");
+	
+	var promise = $.ajax({          
+        type:  'POST',
+        url:   "http://semantic-quran.azure-mobile.net/api/tag",
+        dataType: 'json',
+		data: {
+			tag: val,
+			surah: surahNum,
+			verse: verseNum
+		}
+     });
+	 
+	// Add the tag to our recently added tags
+	var isInList = false;
+	$.each(tagsRecentlyAdded, function(i, entry) {
+		if(entry === val){
+			isInList = true;
+			return false;
+		}
+	});
+	
+	if(!isInList) {
+		tagsRecentlyAdded.unshift(val);
+		tagsRecentlyAdded = tagsRecentlyAdded.slice(0, 10);
+		updateRecentlyAddedTags();
+	}
+	 
+	 return promise;
+}
+
+function doDeleteTag(val, surahNum, verseNum) {
+	console.log("Deleting tag: " + val + " to " + "[" + surahNum + ":" + verseNum + "]");
+	
+	$.ajax({          
+        type:  'DELETE',
+        url:   "http://semantic-quran.azure-mobile.net/api/tag/" + val + "/" + surahNum + "/" + verseNum,
+        dataType: 'json',
+        success: function() {
+			console.log("Successfully Deleted")
+        }
+     });
+}
+
+function updateMRU() {
+	var container = $('#lastUsedTags ul').empty();
+	$.each(tagsMRU, function(i, tag) {
+		var tagElement = $("<li class='tag'></li>").appendTo(container);
+		tagElement.append('<span class="tagName">' + tag + '</span>');
+	});
+	
+	if(typeof(Storage) !== "undefined") {
+		localStorage.tagsMRU = JSON.stringify(tagsMRU);
+	}
+}
+
+function updateRecentlyAddedTags() {
+	var container = $('#recentlyAddedTags').empty();
+	$.each(tagsRecentlyAdded, function(i, tag) {
+		var tagElement = $("<li class='tag recentTag'></li>").appendTo(container);
+		tagElement.append('<span class="tagName">' + tag + '</span>');
+	});
+	
+	if(typeof(Storage) !== "undefined") {
+		localStorage.tagsRecentlyAdded = JSON.stringify(tagsRecentlyAdded);
+	}
+}
+
+function doSearch(val) {
+	window.lastSurah = null;
+	window.enableAutoScroll = false;
+	$('#loadMore').hide();
+    
+	console.log("Doing search for: " + val);
+	var resultPane = $('.resultsPane').empty();
+
+    $.ajax({          
+        type:  'GET',
+        url:   "http://semantic-quran.azure-mobile.net/api/tag/" + val,
+        dataType: 'json',              
+        success: function(data) {
+			if (data && data.length > 0) {
+				loadResults(data, true);
+			}
+        }
+     });
+
+	// Add the search to our recent searches
+	var isInList = false;
+	$.each(tagsMRU, function(i, entry) {
+		if(entry === val){
+			isInList = true;
+			return false;
+		}
+	});
+	
+	if(!isInList) {
+		tagsMRU.unshift(val);
+		tagsMRU = tagsMRU.slice(0, 7);
+		updateMRU();
+	}
+}
+
+function doViewPassage(surah, ayahStart, ayahEnd) {
+	window.enableAutoScroll = true;
+	
+	var resultPane = $('.resultsPane').empty();				
+	loadVerses(surah, ayahStart, ayahEnd, true);
+	
+	window.surah= surah;
+	window.ayahStart = ayahStart || 1;
+	window.ayahEnd = ayahEnd || 50;
+}
+						
+$(function(){									
+	$(window).scroll(function() {
+	   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+	       scrollMore();
+	   }
+	});
+    
+    $('#loadMore').click(scrollMore);
+	
+	function scrollMore() {
+		if (!window.enableAutoScroll) {
+			return;
+		}
+		
+	   window.ayahStart+=50;
+	   window.ayahEnd+=50;
+	   loadVerses(window.surah, window.ayahStart, window.ayahEnd, false);
+	}							
+});
+
+function loadVerses(surah, start, end, animate) {				
+	window.loading = true;
+	var url = "";
+	if (start && end) {
+		url = "http://semantic-quran.azure-mobile.net/api/verse/" + surah + "/" + start + "-" + end;
+	}
+	else {
+		url = "http://semantic-quran.azure-mobile.net/api/verse/" + surah;
+	}
+	
+    $.ajax({          
+        type:  'GET',
+        url:  url,
+        dataType: 'json',              
+        success: function(data) {
+			if (data && data.length > 0) {
+				loadResults(data, animate);
+				if (data.length < 50) {
+					$('#loadMore').hide();
+				}
+				else {
+					$('#loadMore').show();
+				}
+			}
+        }
+     });
+}
+
+function loadResults(data, animate) {
+	$.each(data, function(i, row) {
+		var resultPane = $('.resultsPane');
+		var result = $('<div class="result">');
+		if (animate) {
+			result.fadeIn("slow")
+		}
+		result.appendTo(resultPane);
+		var wrapper = $('<div class="ayahWrapper">')
+			.appendTo(result);
+		var ayahRef = $('<div class="ayahRef"></div>')
+			.text('[' + row.surah + ':' + row.verse + ']')
+			.appendTo(wrapper);
+		var ayah = $('<div class="ayah"></div>')
+			.text(row.quran.text)
+			.appendTo(wrapper);
+		var translation = $('<div class="translation"></div>')
+			.text(row.content[0].text)
+			.appendTo(wrapper);
+		var tagGroup = $('<ul></ul>')
+			.appendTo(wrapper);
+
+		var audioPlayer = $(
+			'<audio controls preload="none">' +
+				'<source src="' + row.audio.mp3.url + '" type="audio/mpeg">' +
+				'<source src="' + row.audio.ogg.url + '" type="audio/ogg">' +
+			'</audio>').appendTo(wrapper);
+		$("<div class='clear'>").appendTo(wrapper);
+		
+		window.lastPlayer;		
+		audioPlayer.on('play', function() {
+			if (window.lastPlayer) {
+				if (window.lastPlayer.pause) {
+					window.lastPlayer.pause();
+				}
+				if (window.lastPlayer[0] && window.lastPlayer[0].pause) {
+					window.lastPlayer[0].pause();
+				}
+				window.lastPlayer.css('width', '40px');
+			}
+			window.lastPlayer = $(this)
+			window.lastPlayer.css('width', '200px');
+		});			
+
+		// Add all the tags to the row
+		$.each(row.tags, function (j, tag) {
+			var tagElement = $("<li class='tag'></li>").prependTo(tagGroup);
+			tagElement.append('<span class="tagName">' + tag + '</span>');
+			
+			var deleteElement = $('<span class="delete">x</span>')
+				.attr("tag", tag)
+				.attr("surah", row.surah)
+				.attr("verse", row.verse);
+				
+			tagElement.append(deleteElement);
+		});
+		
+		var addButton = $('<li class="addTag tag">')
+			.text("+")
+			.attr("surahNum", row.surah)
+			.attr("verseNum", row.verse)
+			.appendTo(tagGroup);
+	});
+	
+	if (animate) {
+		window.scroll(0, 0);
+	}
+}			
+
+window.lastSurah = null;
+function onSurahChanged() {
+		var surah = $("#surahSelect").val();
+		if (surah == window.lastSurah) {
+			return;
+		}
+		window.lastSurah = surah;
+		var ayahStart = $("#ayahStartSelect").val();
+		var ayahEnd = $("#ayahEndSelect").val();
+		doViewPassage(surah, ayahStart, ayahEnd);
+}
+
+(function initializeSurahDropdowns() {
+	var surahSelector = $("#surahSelect");
+	surahSelector.click(onSurahChanged);
+    $.ajax({          
+        type:  'GET',
+        url:   "http://semantic-quran.azure-mobile.net/api/surah",
+        dataType: 'json',
+        success: function(data) {
+			if (data && data.length > 0) {
+				surahSelector.empty();
+				
+				$.each(data, function (i, surahData) {
+					var surahEntry = $('<option value="' + surahData.id + '">' + surahData.id + ': ' + surahData.name.simple + '</option>').appendTo(surahSelector);
+				});
+			}
+        }
+     });
+})();
+
+$(function () {
+	$("#contentBody").on("click", ".tag", function() {
+		var $this = $(this);
+
+		if (!$this.hasClass("addTag") && !$this.hasClass("recentTag")) {
+			var val = $(".tagName", $this).text();
+			doSearch(val);
+		}
+	});
+	
+	$("#contentBody").on("click", "span.delete", function() {
+		var $this = $(this);
+		var tag = $this.attr("tag");
+		var surah = $this.attr("surah");
+		var verse = $this.attr("verse");
+		var parent = $this.parent().remove(); 
+		doDeleteTag(tag, surah, verse);
+		
+		return false;
+	});
+	
+	$("#addTagDialogButton").click(function () {
+		var $textBox = $("#addTagDialogTextBox");
+		var tags = $textBox.val();
+		var surahNum = context.attr("surahNum");
+		var verseNum = context.attr("verseNum");
+		
+		$textBox.val('');
+		
+		if (tags != null && tags.length>0) {
+			var values = tags.split(/[,;]/);
+			$.each(values, function (i, value) {
+				doAddTag(value, surahNum, verseNum).done(function (val) {
+					console.log("Successfully Added: " + val.text);
+					
+					// Update the local row
+					var tagGroup = context.parent();					
+					var tagElement = $("<li class='tag'></li>").prependTo(tagGroup);
+					tagElement.append('<span class="tagName">' + val.text + '</span>');
+					
+					var deleteElement = $('<span class="delete">x</span>')
+						.attr("tag", val.text)
+						.attr("surah", surahNum)
+						.attr("verse", verseNum);
+						
+					tagElement.append(deleteElement);
+				});
+			});				
+		}
+		
+		$("#addTagDialog").popup("close");					
+		return false;
+	});
+	
+	$("#contentBody").on("click", ".addTag", function() {
+		context = $(this);
+		var textBox = $("#addTagDialogTextBox").val(""); 
+		var dialog = $("#dialogLink").click();
+		setTimeout(function () {
+			textBox.focus();
+		}, 500);
+	});
+	
+	$("#recentlyAddedTags").on("click", ".recentTag", function() {
+		var $this = $(this);
+		var $textBox = $("#addTagDialogTextBox");
+		var val = $(".tagName", $this).text();
+        var existing = $textBox.val();
+        if (existing) {
+            val = existing + ',' + val;
+        }
+		$textBox.val(val);
+	});
+	
+	$("#searchButton").click(function() {
+		var val = $("#search-tag").val();
+		
+		if (val && val.length > 0) {
+			doSearch(val);
+		}
+	});
+	
+	(function onSurahChanged() {
+		var surah = $("#surahSelect").val();
+		var ayahStart = $("#ayahStartSelect").val();
+		var ayahEnd = $("#ayahEndSelect").val();
+		doViewPassage(surah, ayahStart, ayahEnd);
+	})();
+	
+	if(typeof(Storage) !== "undefined") {
+		try {
+		tagsMRU = JSON.parse(localStorage.tagsMRU);
+		tagsRecentlyAdded = JSON.parse(localStorage.tagsRecentlyAdded);
+		updateMRU();
+		updateRecentlyAddedTags();
+		}
+		catch (e) {
+			console.log('failed to read local storage', e);
+		}
+	}
+});
+
+var client = new WindowsAzure.MobileServiceClient('https://semantic-quran.azure-mobile.net/', 'okajHbuHsfhRmylXmwQgOKAsnmUyKG49');
+$(function(){
+	var loginBtn = $("#login");
+	loginBtn.click(function(){
+		client.login("facebook").done(function (results) {
+			loginBtn.hide();
+	    }, function (err) {
+	         alert("Error: " + err);
+	    });
+	});
+});
