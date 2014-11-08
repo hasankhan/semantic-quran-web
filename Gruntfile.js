48ad7336245dfd526ef9d2cde0598deea961b762
Hasan Khan <slashdot10@gmail.com>
2014-11-07 18:31:41 -0800

    Modified Gruntfile.js to exclude the output file from input list to avoid recursive concat of output

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,23 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'js/*.js',
-        dest: 'js/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: ['js/*.js', '!js/<%= pkg.name %>.min.js'],
+                dest: 'js/<%= pkg.name %>.min.js'
+            }
+        }
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
 
 };
